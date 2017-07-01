6f0255a4eeb5bdf334b95f48d4c8a0abfc787c41
Owen <angrychairr@googlemail.com>
2017-07-01 13:11:21 +0200

    Fixed merge fail
    I suck at merging

--- public/js/socketclient.js
+++ public/js/socketclient.js
@@ -18,23 +18,6 @@ class NetworkClient
 
         var socket = io("80.255.0.159:7990", connectionOptions);
 
-        var imgData = (JSON.parse(data.data));
-        var ctx = $(".game-canvas")[0].getContext("2d");
-        cimgd = ctx.createImageData(100, 100);
-
-        var dataIndex = 0;
-        var cimgdSize = 100 * 100 * 4;
-
-        for(var i = 0; i < cimgdSize; i+=4)
-        {
-            cimgd.data[i] = imgData[dataIndex++];
-            cimgd.data[i+1] = imgData[dataIndex++];
-            cimgd.data[i+2] = imgData[dataIndex++];
-            cimgd.data[i+3] = 255;
-        }
-
-        ctx.putImageData(cimgd, 20, 20);
-
         $(window).resize(function()
         {
             var cvs = $(".game-canvas")[0];
