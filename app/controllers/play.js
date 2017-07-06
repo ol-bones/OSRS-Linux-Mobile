b859e90898a2d44b8ead5acbcb9b8eb0791ae83d
Owen <angrychairr@googlemail.com>
2017-07-06 20:19:53 +0200

    requestNode test

--- app/controllers/play.js
+++ app/controllers/play.js
@@ -16,6 +16,39 @@ exports.play = function (req, res)
     res.render('play');
 };
 
+exports.requestNode = function(req, res)
+{
+    var headers =
+    {
+        "User-Agent": "WebFront",
+        "Content-Type": "application/x-www-form-urlencoded"
+    };
+
+    var addr = req.connection.remoteAddress.toString().replace(/^.*:/, '');
+    console.log(addr);
+    var options =
+    {
+        url: "http://80.255.0.159:9000/spawnStreamer",
+        method: "POST",
+        headers: headers,
+        form: {"Address": addr, "User": "UrMum"}
+    };
+
+    request(options, function(err, nodeRes, body)
+    {
+        if(!err && nodeRes.statusCode === 200)
+        {
+            console.log(body);
+            res.send(nodeRes);
+        }
+        else if(err || nodeRes.statusCode !== 200)
+        {
+            console.error(err || nodeRes.statusCode);
+            res.status((err) ? 404 : nodeRes.statusCode).send("ERR");
+        }
+    });
+};
+
 exports.connect = function(req, res)
 {
     res.json(req.g_ServerList.getSuitableNode());
